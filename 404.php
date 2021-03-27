4503f245eec8e429ca30b2d6783fe47433723c61
OualidHamdi <oualidhamdi12@gmail.com>
2021-03-27 22:45:33 +0100

    DevBlog

--- 404.php
+++ 404.php
@@ -21,19 +21,12 @@
   <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
   <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
 
-  <!-- swiper -->
-  <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
-  <link rel="stylesheet" href="css/swiper.css">
 
 
   <!-- Custom styles for this template -->
   <link href="css/clean-blog.min.css" rel="stylesheet">
 
-  <style type="text/css">
-    .swiper-button-next, .swiper-button-prev{
-      
-    }
-  </style>
+
 </head>
 
 <body>
@@ -75,95 +68,21 @@
   </nav>
 
   <!-- Page Header -->
-  <header class="masthead" style="background-image: url('img/home-bg.jpg')">
+  <header class="masthead" style="background-image: url('img/404.jpg')">
     <div class="overlay"></div>
     <div class="container">
       <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
           <div class="site-heading">
-            <h1>< / ></h1>
-            <span class="subheading">All new in informational development.</span>
+            <h1>404</h1>
+            <span class="subheading">page not found</span>
           </div>
         </div>
       </div>
     </div>
   </header>
 
-    <!-- Swiper -->
-  <div class="swiper-container">
-    <div class="swiper-wrapper">
-            <?php
-         $query = "SELECT * FROM category ORDER BY ID DESC";
-    $query_run = mysqli_query($connection,$query);
 
-       if(mysqli_num_rows($query_run)>0)
-      {
-        while($row = mysqli_fetch_assoc($query_run))
-        {
-    
-          ?>
-      <div class="swiper-slide"><?php echo $row['name']; ?></div>
-       <?php
-        }
-      }else {
-        header ('Location: 404.php');
-      }
-    ?>
-
-    </div>
-    <!-- Add Pagination -->
-    <div class="swiper-pagination"></div>
-    <!-- Add Arrows -->
-    <div class="swiper-button-next"></div>
-    <div class="swiper-button-prev"></div>
-  </div>
-
-  <!-- Main Content -->
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-8 col-md-10 mx-auto">
-
-        <?php
-         $query = "SELECT * FROM post ORDER BY ID DESC";
-    $query_run = mysqli_query($connection,$query);
-
-       if(mysqli_num_rows($query_run)>0)
-      {
-        while($row = mysqli_fetch_assoc($query_run))
-        {
-    
-          ?>
-        <div class="post-preview">
-          <a href="post.html">
-            <h2 class="post-title">
-              <?php echo $row['title']; ?>
-            </h2>
-            <h3 class="post-subtitle">
-             <?php echo $row['subtitle']; ?>
-            </h3>
-          </a>
-          <p class="post-meta">Posted by
-            <a href="#"><?php echo $row['auteurID']; ?></a>
-            on<?php echo $row['publishedat']; ?></p>
-        </div>
-        <hr>
- <?php
-        }
-      }else {
-           header ('Location: 404.php');
-      }
-    ?>
-  
-        
-        <!-- Pager -->
-        <div class="clearfix">
-          <a class="btn btn-primary float-right" href="#">Older Posts &rarr;</a>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <hr>
 
   <!-- Footer -->
   <footer>
@@ -208,10 +127,6 @@
 
   <!-- Custom scripts for this template -->
   <script src="js/clean-blog.min.js"></script>
-
-    <!-- Swiper JS -->
-  <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
-  <script src="js/swiper.js"></script>
 
 </body>
 
